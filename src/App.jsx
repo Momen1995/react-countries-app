c9c870e8dd727c33dae6ca6dde30f2c5e3da6913
Momen1995 <momensrk@gmail.com>
2024-01-31 21:04:51 +0600

    mark as visited add

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Country from './Components/Country'
+import { list } from 'postcss'
 
 function App() {
   const [countries,setCountries] = useState([])
+  const [visitedCountries,setVisitedCountries] = useState([])
 
   useEffect(()=>{
     fetch("https://restcountries.com/v3.1/all")
     .then(res=>res.json())
     .then(data=>setCountries(data))
   },[])
+
+  const handleVisitedCountries = (country) =>{
+    const newVisitedCountry = [...visitedCountries,country];
+    setVisitedCountries(newVisitedCountry)
+  }
   return (
     <>
       <h3 className='text-3xl text-center uppercase my-7'>React Rest Countries</h3>
-      <div className='grid grid-cols-4 gap-8 '>
+      <h2>Country Length: {countries.length}</h2>
+      <div>
+        <h3>Visited Countries: {visitedCountries.length}</h3>
+        <ul className="list-decimal pl-5">
+          {
+            visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+          }
+        </ul>
+      </div>
+      <div className='grid grid-cols-4 gap-4 '>
         {
-          countries.map(country =><Country  country={country}></Country>)
+          countries.map(country =><Country key={country.cca3}  country={country} handleVisitedCountries={handleVisitedCountries}></Country>)
         }
       </div>
     </>
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,12 +1,36 @@
-const Country = ({country}) => {
-  const { name, flags, maps, population, postalCode } = country;
+import { useState } from "react";
+
+const Country = ({ country, handleVisitedCountries }) => {
+  const { name, flags, maps, population, postalCode, area, cca3 } = country;
+
+  const [visit, setVisit] = useState(false);
+
+  const handleVisit = () => {
+    setVisit(!visit);
+  };
   return (
-    <div className="bg-slate-200 p-3 mt-5 rounded space-y-1">
+    <div
+      className={`${
+        visit ? "bg-red-400" : "bg-slate-200"
+      } p-4 mt-5 rounded space-y-1`}
+    >
       <img src={flags.png} alt={flags.alt} className="w-58 h-48" />
       <h3>Name: {name.common}</h3>
       <p>Official: {name.official}</p>
       <div>
         <p>Population: {population}</p>
+        <p>Area: {area}</p>
+        <p>Code: {cca3}</p>
+        <button onClick={handleVisit} className="bg-teal-600 px-4 py-2 rounded">
+          {visit ? "Visit" : "Going"}
+        </button>
+        {visit ? "visited" : "Not visited"}
+        <button
+          className="bg-yellow-600 px-4 py-2 rounded mt-5"
+          onClick={()=>handleVisitedCountries(country)}
+        >
+          Mark as visited
+        </button>
       </div>
     </div>
   );
